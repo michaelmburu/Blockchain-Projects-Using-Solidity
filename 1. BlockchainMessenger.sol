87ca20b2b3eb7ff26e5509f6bc6033e195e4429c
Michael Mburu <ichangaimichael@yahoo.com>
2023-03-25 23:21:01 +0300

    - Payable Blockchain Messenger Solidity Contract

--- 1. BlockchainMessenger.sol
+++ 1. BlockchainMessenger.sol
@@ -6,15 +6,21 @@ contract TheBlockChainMessenger {
     uint public changeCounter;
     address public owner;
     string public theMessage;
-
+    uint contractAmount = address(this);
     constructor() {
         owner = msg.sender;
     }
 
-    function updateTheMessage(string memory _newMessage) public {
-        if(msg.sender == owner){
-            theMessage = _newMessage;
-            changeCounter++;
+    function updateTheMessage(string memory _newMessage) public payable {
+        if(msg.value > 1 ether){
+            if(msg.sender == owner){
+                        theMessage = _newMessage;
+                        changeCounter++;
+                    }
+       
+        }
+        else {
+            payable(msg.sender).transfer(msg.value);
         }
        
     }
